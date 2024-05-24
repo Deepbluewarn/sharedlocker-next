fe36f81c99f3ed937fe105b92c4e3475f94bc5ed
bluewarn <n5lp97@gmail.com>
2024-05-24 06:37:05 +0000

    인터페이스 이름 수정

--- src/actions/locker/getLockerList.ts
+++ src/actions/locker/getLockerList.ts
@@ -1,8 +1,8 @@
 import IApiResponse from "@/interfaces/api";
 import FetchWrapper from "../fetch-wrapper";
-import { ILockerInfoList } from "@/interfaces/api/locker";
+import { ILockerInfo } from "@/interfaces/api/locker";
 
-export async function fetchLockerList(buildingNumber: number, floor: number): Promise<IApiResponse<string, ILockerInfoList>> {
+export async function fetchLockerList(buildingNumber: number, floor: number): Promise<IApiResponse<string, ILockerInfo[]>> {
     return FetchWrapper(`${process.env.API_BASE_URL}/api/lockers?buildingNumber=${buildingNumber}&floor=${floor}`, {
         method: 'GET',
     }).then(res => res.json())
--- src/interfaces/api/locker/index.ts
+++ src/interfaces/api/locker/index.ts
@@ -29,7 +29,7 @@ export interface ILockerDetail {
     shareRequestedUsers: IUserInfo[]
 }
 
-export interface ILockerInfoList {
+export interface ILockerInfo {
     lockerNumber: number
     status: string
 }
