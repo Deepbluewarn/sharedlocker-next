a7be29208019e2f3a1ea6056aeadefbb384c8be2
bluewarn <n5lp97@gmail.com>
2024-05-30 05:18:26 +0000

    500 이하 상태 코드는 에러를 던지지 않음

--- src/actions/fetch-wrapper.ts
+++ src/actions/fetch-wrapper.ts
@@ -2,7 +2,7 @@ export default async function FetchWrapper(url: string, options: RequestInit) {
     try {
         const res = await fetch(url, options)
 
-        if(res.ok) {
+        if(res.status < 500) {
             return res // 사용자가 원하는 형태로 변환하지 않고 그대로 반환
         } else {
             const error_res = await res.json()
